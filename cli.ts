ae6bcc8634c2ebc74ac961179fefb7ec83c47a5a
Marcin Puc <marcin.e.puc@gmail.com>
2022-03-11 09:20:33 +0100

    style: remove unnecessary semicolon from shell script

--- cli.ts
+++ cli.ts
@@ -3,7 +3,7 @@ import { writeAll } from "std/streams/conversion.ts";
 const script = `
 T='gYw'
 echo -e "\n                 40m     41m     42m     43m\
-     44m     45m     46m     47m";
+     44m     45m     46m     47m"
 for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' \
            '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' \
            '  36m' '1;36m' '  37m' '1;37m'; do
